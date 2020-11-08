1359d87a7df72d02661fb93b209af98d985a283a
Pranav Balaji <pranavbalaji01@gmail.com>
2020-11-08 16:19:46 +0530

    Promises Part-1

--- Front-End Javascript Frameworks - Angular/conFusion/src/app/home/home.component.ts
+++ Front-End Javascript Frameworks - Angular/conFusion/src/app/home/home.component.ts
@@ -19,13 +19,16 @@ export class HomeComponent implements OnInit {
   leader: Leader;
 
   constructor(private dishService: DishService,
-    private promotionService: PromotionService,
-    private leaderService: LeaderService) { }
+              private promotionService: PromotionService,
+              private leaderService: LeaderService) { }
 
   ngOnInit(): void {
-    this.dish = this.dishService.getFeaturedDish();
-    this.promotion = this.promotionService.getFeaturedPromotion();
-    this.leader  = this.leaderService.getFeaturedLeader();
+    this.dishService.getFeaturedDish()
+      .then(dish => this.dish = dish);
+    this.promotionService.getFeaturedPromotion()
+      .then(promotion => this.promotion = promotion);
+    this.leaderService.getFeaturedLeader()
+      .then(leader => this.leader = leader);
   }
 
 }
