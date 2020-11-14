2da82c011b43dae56666ae09ee79bccecbbb7695
Pranav Balaji <pranavbalaji01@gmail.com>
2020-11-14 18:03:39 +0530

    express router

--- Server-side Development with NodeJS, Express and MongoDB/node-express/index.js
+++ Server-side Development with NodeJS, Express and MongoDB/node-express/index.js
@@ -1,15 +1,19 @@
 import express from "express";
 import morgan from "morgan";
 import path from "path";
+import bodyParser from "body-parser";
 import http from "http";
+
+import dishRouter from "./routes/dishRouter.js";
 
 const host = "localhost";
 const port = 3000;
 
 const app = express();
 app.use(morgan("dev"));
-
+app.use(bodyParser.json());
 app.use(express.static(path.resolve() + "/public"));
+app.use("/dishes", dishRouter);
 
 app.use((req, res, next) => {
   res.statusCode = 200;
